7811950cdbce8480cf4d896183aa840553d593a2
Fernandomartines1989 <114265995+Fernandomartines1989@users.noreply.github.com>
2023-03-13 21:51:03 -0300

    Update app.py

--- app.py
+++ app.py
@@ -5,3 +5,11 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
   return "Olá, mundo! Esse é meu site. (Fernando Martines)"
+
+@app.route("/sobre")
+def sobre():
+  return "Meu nome é Fernando Martines, sou jornalista"
+
+@app.route("/contato")
+def contato():
+  return "fernandomartines0@gmail.com"
