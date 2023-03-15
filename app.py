3e8a03243b72ed5fa0cc109a0c285382da8e4391
Fernandomartines1989 <114265995+Fernandomartines1989@users.noreply.github.com>
2023-03-15 20:56:07 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,13 +4,9 @@ app = Flask(__name__)
 
 menu = """ <a href="/">Página inicial</a> | <a href="/sobre">Sobre</a> | <a href="/contato">Contato</a> <br> """
 
-requisicao = requests.get("https://www.bcb.gov.br/api/servico/sitebcb/agendadiretoria?lista=Agenda%20da%20Diretoria&inicioAgenda=%272023-02-27%27&fimAgenda=%272023-02-27%27")
-requisicao.content
-html = BeautifulSoup(requisicao.content)
-
 @app.route("/")
 def hello_world():
-  return menu + "Agenda do BC" + html
+  return menu + "Bem-vindo ao meu site"
 
 @app.route("/sobre")
 def sobre():
