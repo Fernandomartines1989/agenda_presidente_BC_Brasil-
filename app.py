a0f0bb3707e14deb5f485406e3cb6faf8abf1c4e
Fernandomartines1989 <114265995+Fernandomartines1989@users.noreply.github.com>
2023-03-29 20:33:28 -0300

    Update app.py

--- app.py
+++ app.py
@@ -47,6 +47,6 @@ def telegram_bot():
   update = request.json
   chat_id = update["message"]["chat"]["id"]
   message = update["message"]["text"]
-  nova_mensagem = {"chat_id": chat_id, "text": message}
+  nova_mensagem = {"chat_id": chat_id, "text": agenda_BC}
   requests.post(f"https://api.telegram.org./bot{TELEGRAM_API_KEY}/sendMessage", data=nova_mensagem)
   return "ok"
