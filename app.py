bf3c6ec4fbbadba5185f7f8a461dfb4146f964e7
Fernandomartines1989 <114265995+Fernandomartines1989@users.noreply.github.com>
2023-03-30 18:25:28 -0300

    Update app.py

--- app.py
+++ app.py
@@ -50,11 +50,11 @@ def telegram_bot():
   chat_id = update["message"]["chat"]["id"]
   message = update["message"]["text"]
   nova_mensagem = {"chat_id": chat_id, "text": agenda_BC}
-  mensagem_if = {"chat_id": chat_id, "text": f"Olá! Seja bem-vindo (a). Quer saber a agenda do Presidente do Banco Central do Brasil de {hoje}? Digite sim (tudo minúsculo)" }
+  mensagem_if = {"chat_id": chat_id, "text": f"Olá! Seja bem-vindo (a). Quer saber a agenda do Presidente do Banco Central do Brasil de {hoje}? Digite Sim" }
   mensagem_else = {"chat_id": chat_id, "text": "Não entendi Digite /start e eu te digo o que sei fazer" }
   if message == "/start":
     texto_resposta = requests.post(f"https://api.telegram.org./bot{TELEGRAM_API_KEY}/sendMessage", data=mensagem_if)
-  elif message == "sim":
+  elif message.lower() == "sim":
     texto_resposta = requests.post(f"https://api.telegram.org./bot{TELEGRAM_API_KEY}/sendMessage", data=nova_mensagem)
   else:
     texto_resposta = requests.post(f"https://api.telegram.org./bot{TELEGRAM_API_KEY}/sendMessage", data=mensagem_else)
